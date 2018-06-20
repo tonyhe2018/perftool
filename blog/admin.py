ce6485b3b7deff733d3c7b925f19ebf2b60cf06f
root <root@linux-pihc.localdomain>
2018-06-20 20:47:40 +0800

    add Aricle

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from blog.models import Blog
+from blog.models import Blog, Article
 
 # Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title','pub_data','update_time')
+
+
 admin.site.register(Blog)
+admin.site.register(Article, ArticleAdmin)
--- blog/models.py
+++ blog/models.py
@@ -9,3 +9,12 @@ class Blog(models.Model):
 
     def __str__(self):
         return self.title
+
+class Article(models.Model):
+    title = models.CharField('title', max_length = 50)
+    content = models.TextField('content')
+    pub_data = models.DateTimeField('publish date', auto_now_add = True, editable = True)
+    update_time = models.DateTimeField('update date', auto_now = True, null = True)
+
+    def __str__(self):
+        return self.title
